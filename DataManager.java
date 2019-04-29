d319cdaa59d4f06f3cd2fdd6dda575455fd593ee
Jessica <jessica-chu@users.noreply.github.com>
2019-04-29 16:36:36 -0400

    Updated DataManager

--- DataManager.java
+++ DataManager.java
@@ -6,17 +6,17 @@ public class DataManager{
 	private static Connection connection;
     private static Statement statement;
     
-    public static void main(String[] args){
+    static{
         //connects to the database when loaded
         try{
             //credentials
             String hostLoc = "jdbc:mysql://localhost:3306/";
             String user = "root";
-            String password = "cody1234";
+            String password = "3821";
             //credentials
 
             //database & tables
-            String createDatabase = "CREATE DATABASE IF NOT EXISTS NAServer;";
+            String createDatabase = "CREATE DATABASE IF NOT EXISTS NAserver;";
 
             String createUserTable = "CREATE TABLE IF NOT EXISTS User("
                     + "username VARCHAR(128) PRIMARY KEY NOT NULL,"
@@ -51,7 +51,109 @@ public class DataManager{
         }
     }
 
-	public static void shutdown(){
+    //for profile page
+    public static String [] getPersonalInfo(String username){
+        String [] personalInfo = {"",""};
+
+        try{
+            String selectUserInfo = "SELECT name,address FROM User WHERE username=\"" +username+ "\";";
+
+            ResultSet userInfo = statement.executeQuery(selectUserInfo);
+
+            if(userInfo.next()){
+                personalInfo[0] = userInfo.getString("name");
+                personalInfo[1] = userInfo.getString("address");
+
+                userInfo.close();
+
+                return personalInfo;
+            }
+
+        }catch(Exception expt){
+            expt.printStackTrace();
+        }
+
+        return (personalInfo);
+    }
+
+    //create new user
+    public static void createNewUser(String username, String password, String name, String address, String phonenum,
+                                     String credcard){
+        if(username.equals(""))
+            return;
+        try{
+            String insertNewUser = "INSERT IGNORE INTO User VALUES(\"" + username + "\",\"" + password + "\",\""
+                    + name + "\",\"" + address + "\",\"" + phonenum + "\",\"" + credcard + "\");";
+            statement.executeUpdate(insertNewUser);
+
+        }catch(Exception expt){
+            expt.printStackTrace();
+        }
+    }
+
+    //checks if username exists
+
+    public static boolean isValidAdmin(String username, String password){
+        try{
+            int numberOfAdmins = 0;
+            String countAdminQuery = "SELECT COUNT(1) FROM Admin WHERE username=\"" +username+ "\" AND password=\"" +password+ "\";";
+            ResultSet countInfo = statement.executeQuery(countAdminQuery);
+
+            if(countInfo.next()){
+                numberOfAdmins = countInfo.getInt("COUNT(1)");
+            }
+            countInfo.close();
+            return numberOfAdmins > 0;
+
+        }catch(Exception expt){
+            expt.printStackTrace();
+        }
+        return false;
+    }
+
+    public static boolean isValidAdminName(String username){
+        if(username.equals(""))
+            return false;
+        try{
+            int numberOfUsernames = 0;
+            String countUsernames = "SELECT COUNT(1) FROM Admin WHERE username=\"" + username + "\";";
+            ResultSet countInfo = statement.executeQuery(countUsernames);
+
+            if(countInfo.next()){
+                numberOfUsernames = countInfo.getInt("COUNT(1)");
+            }
+
+            return numberOfUsernames > 0;
+
+        }catch(Exception expt){
+            expt.printStackTrace();
+        }
+        return false;
+    }
+
+    public static boolean isValidUsername(String username){
+        if(isValidAdminName(username))
+            return true;
+        if(username.equals(""))
+            return false;
+        try{
+            int numberOfUsernames = 0;
+            String countUsernames = "SELECT COUNT(1) FROM User WHERE username=\"" + username + "\";";
+            ResultSet countInfo = statement.executeQuery(countUsernames);
+
+            if(countInfo.next()){
+                numberOfUsernames = countInfo.getInt("COUNT(1)");
+            }
+
+            return numberOfUsernames > 0;
+
+        }catch(Exception expt){
+            expt.printStackTrace();
+        }
+        return false;
+    }
+
+    public static void shutdown(){
 		try{
 			if(connection!= null)
 				connection.close();
