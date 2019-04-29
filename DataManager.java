bde7d2a62af6b2fb63eeb1cec710212264b7e3f8
codychen <cchen008@citymail.cuny.edu>
2019-04-29 15:47:55 -0400

    Create User

--- DataManager.java
+++ DataManager.java
@@ -2,25 +2,41 @@ import java.sql.*;
 import java.util.*;
 
 public class DataManager{
-
-	private static Connection connection;
+    
+    private static Connection connection;
     private static Statement statement;
     
-    static{
-    	//connects to the database when loaded
-    	try{
-    		//credentials
-    		String hostLoc = "jdbc:mysql://localhost:3306/";
-    		String user = "root";
-    		String password = "@Fcrt39jiv9";
-    		//credentials
-    		
-    		//database & tables
-    		 String createDatabase = "CREATE DATABASE IF NOT EXISTS NAServer;";
-    		//database & tables
-    	}
-    	catch(Exception e){
-    		e.printStackTrace();
-    	}
+    public static void main(String[] args){
+        //connects to the database when loaded
+        try{
+            //credentials
+            String hostLoc = "jdbc:mysql://localhost:3306/";
+            String user = "root";
+            String password = "cody1234";
+            //credentials
+            
+            //database & tables
+            String createDatabase = "CREATE DATABASE IF NOT EXISTS NAServer;";
+            //database & tables
+            
+            String createUserTable = "CREATE TABLE IF NOT EXISTS User("
+            + "username VARCHAR(128) PRIMARY KEY NOT NULL,"
+            + "password VARCHAR(128),"
+            + "name VARCHAR(128),"
+            + "address VARCHAR(128));";
+            
+            connection = DriverManager.getConnection(hostLoc,user,password);
+            statement = connection.createStatement();
+            
+            statement.executeUpdate(createDatabase);
+            
+            connection = DriverManager.getConnection(hostLoc+"NAServer",user,password);
+            statement = connection.createStatement();
+            
+            statement.executeUpdate(createUserTable);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
